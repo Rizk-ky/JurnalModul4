df685ffb6bcff1ac20f64dfe6ff8702fb8266381
ravinhub <muhammadrizkyramadhan365@gmail.com>
2026-03-18 05:23:48 +0700

    implementasi table driven

--- JurnalModul4/Program.cs
+++ JurnalModul4/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using JurnalModul4;
+class Program
+{
+    static void Main(string[] args)
+    {
+        TableDriven kb = new TableDriven();
+
+        Console.WriteLine(kb.getKodeBuah("Apel"));
+        Console.WriteLine(kb.getKodeBuah("Durian"));
+        Console.WriteLine(kb.getKodeBuah("Mangga")); 
+
+        Console.WriteLine();
+
+    }
+}
---
+++ JurnalModul4/TableDriven.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace JurnalModul4
+{
+    class TableDriven
+    {
+        string[] namaBuah = {
+        "Apel","Aprikot","Alpukat","Pisang","Paprika",
+        "Blackberry","Ceri","Kelapa","Jagung",
+        "Kurma","Durian","Anggur","Melon","Semangka"
+    };
+
+        string[] kodeBuah = {
+        "A00","B00","C00","D00","E00",
+        "F00","H00","I00","J00",
+        "K00","L00","M00","N00","O00"
+    };
+
+        public string getKodeBuah(string input)
+        {
+            for (int i = 0; i < namaBuah.Length; i++)
+            {
+                if (namaBuah[i] == input)
+                {
+                    return kodeBuah[i];
+                }
+            }
+
+            return "Kode buah tidak ditemukan";
+        }
+    }
+}
